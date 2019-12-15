7a49714cfd026b89b34e7ec2b6749ceac1f72374
LucasZack <Lucas.Zacarias@etec.sp.gov.br>
2019-12-15 16:51:54 -0300

    Aula-8 de node.js: Conexão com o banco de dados

--- Aula-8/test.js
+++ Aula-8/test.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('sistemadecadastro', 'root', 'Lo47810144', {
   host: "localhost",
-  dialect: 'mysql',
+  dialect: 'mysql'
 });
 
 sequelize.authenticate().then(function(){
