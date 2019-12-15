e9ed5f75eef36f83023a63fa9a9ab6b231b9df3c
LucasZack <Lucas.Zacarias@etec.sp.gov.br>
2019-12-15 16:14:57 -0300

    Aula-8 de Node.js: Conexão com banco de dados

--- Aula-8/Index.js
+++ Aula-8/Index.js
@@ -6,14 +6,13 @@ app.get('/', function(req, res){
 });
 
 app.get('/sobre', function(req, res){
-  res.sendFile(__dirname + '/html/')
+  res.sendFile(__dirname + '/html/sobre.html')
 })
 
 app.get('/blog', function(req, res){
   res.send("Seja bem-vindo ao meu site!");
 });
 
-
-app.listen(8040), function() {
+app.listen(8040, function() {
   console.log("Servidor rodando na url ' localhost:8040 '");
-}
+});
